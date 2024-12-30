f3afde7d7f5c1bd6bd0d3101a281370e094de7bc
nimanthijayathilake678 <jayathilake.nimanthi99@gmail.com>
2024-12-30 12:24:22 +0530

    Update User class implemets UserDetails

--- auth-service/src/main/java/SongNest/auth_service/model/User.java
+++ auth-service/src/main/java/SongNest/auth_service/model/User.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "user")
-public class User  {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -28,6 +28,79 @@ public class User  {
     @Column(name="subscription_plan")
     private String SubscriptionPlan;
 
+    @Enumerated(value=EnumType.STRING)
+    Role role;
 
+    public Integer getUserID() {
+        return UserID;
+    }
 
+    public void setUserID(Integer userID) {
+        UserID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getSubscriptionPlan() {
+        return SubscriptionPlan;
+    }
+
+    public void setSubscriptionPlan(String subscriptionPlan) {
+        SubscriptionPlan = subscriptionPlan;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
