ca320504a35cb106286bfb378f22a14196731647
nimanthijayathilake678 <jayathilake.nimanthi99@gmail.com>
2024-12-31 14:09:03 +0530

    Update UserRepository

---
+++ auth-service/src/main/java/SongNest/auth_service/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package SongNest.auth_service.repository;
+
+import SongNest.auth_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
