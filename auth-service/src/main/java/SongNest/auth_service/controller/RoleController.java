c7ce64585399695b9739040513301817e0bdf2b0
nimanthijayathilake678 <jayathilake.nimanthi99@gmail.com>
2025-01-02 11:18:43 +0530

    Update Role controller

--- auth-service/src/main/java/SongNest/auth_service/controller/RoleController.java
+++ auth-service/src/main/java/SongNest/auth_service/controller/RoleController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public class RoleController {
+
     @Autowired
     private RoleService roleService;
 
